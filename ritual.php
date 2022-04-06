5602097df196257144b1ba0aeeb7f710f04387da
itisj <63428885+itisj@users.noreply.github.com>
2022-04-06 14:20:49 +1000

    ritual

---
+++ ritual.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+
+Have <?php echo $_POST["have"]; ?><br>
+Know <?php echo $_POST["know"]; ?>
+Find <?php echo $_POST["find"]; ?>
+Call <?php echo $_POST["call"]; ?>
+Tear apart <?php echo $_POST["tear"]; ?>
+Fear till <?php echo $_POST["fear"]; ?>
+
+
+</body>
+</html>
